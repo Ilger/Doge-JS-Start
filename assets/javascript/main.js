54f73278edd02a7c159be6b0cbbaffa6349e1ae5
Ilger <ilgerdepraetere@gmail.com>
2019-05-28 09:55:15 +0200

    add new main.js with the external js

---
+++ assets/javascript/main.js
@@ -0,0 +1,13 @@
+// document.getElementById("myFrame").addEventListener("load", myFunction);
+// function myFunction(){
+//   document.getElementById("doge").innerHTML = "Woof";
+// };
+// function Woof () {
+//   document.getElementById("doge").innerHTML = "Woof";
+// }
+// var dogeButton = document.getElementById("doge");
+// var
+//
+window.onload = function () {
+    document.getElementById("doge").innerHTML = "Woof"
+  };
