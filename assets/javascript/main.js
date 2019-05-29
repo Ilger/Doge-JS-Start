b88a05bf57b4913ccc020d33c7dde4e6257da918
Ilger <ilgerdepraetere@gmail.com>
2019-05-29 15:58:44 +0200

    add comment to js code currnetly in file

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,20 @@
+
+// declaere button id
 let doge = document.getElementById('doge');
 
+// onload change button text to 'Woof'
 let Woof = function() {
   doge.innerHTML = 'Woof';
 }
 
 window.onload = Woof;
 
+// on button click change button text to 'Woof woof!'
 let WoofWoof = function(){
   doge.innerHTML = 'Woof woof!';
 }
-
+// changed it to onmousedown to make it a more snappy experience
 doge.onmousedown = WoofWoof;
+
+
+// document.createElement("img");
